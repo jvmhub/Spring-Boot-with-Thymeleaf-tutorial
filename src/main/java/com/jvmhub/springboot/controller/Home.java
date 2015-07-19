fe61182446f1155a3a37335df147b43f4d86771a
hubek <niewiem15@op.pl>
2015-07-19 21:39:32 +0200

    Spring Boot with Thymeleaf

---
+++ src/main/java/com/jvmhub/springboot/controller/Home.java
@@ -0,0 +1,13 @@
+package com.jvmhub.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
